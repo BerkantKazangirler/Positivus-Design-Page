b0baaed02ecf3b4c097add3440b92189373ef423
BerkantKazangirler <berkantkazangirler@gmail.com>
2024-09-03 14:14:32 +0300

    feat: added Testimonials area

--- src/App.tsx
+++ src/App.tsx
@@ -491,6 +491,50 @@ function App() {
             about Our Digital Marketing Services
           </p>
         </div>
+        <div className="bg-main-gray flex flex-row mt-32 rounded-2xl w-full">
+          <div className="hidden flex-col p-8 -left-20 relative md:flex">
+            <p className="relative max-w-128 p-10 rounded-2xl border border-main-green text-white">
+              "We have been working with Positivus for the past year and have
+              seen a significant increase in website traffic and leads as a
+              result of their efforts. The team is professional, responsive, and
+              truly cares about the success of our business. We highly recommend
+              Positivus to any company looking to grow their online presence."
+              <span className="absolute w-[1rem] h-[1rem] left-10 -bottom-2 rotate-45 border-b border-r border-main-green bg-main-gray"></span>
+            </p>
+            <div className="flex flex-col mt-5 mx-11">
+              <span className="text-main-green">John Smith</span>
+              <span className="text-white">Marketing Director at XYZ Corp</span>
+            </div>
+          </div>
+          <div className="flex flex-col p-8 mx-auto">
+            <p className="relative max-w-128 p-10 rounded-2xl border border-main-green text-white">
+              "We have been working with Positivus for the past year and have
+              seen a significant increase in website traffic and leads as a
+              result of their efforts. The team is professional, responsive, and
+              truly cares about the success of our business. We highly recommend
+              Positivus to any company looking to grow their online presence."
+              <span className="absolute w-[1rem] h-[1rem] left-10 -bottom-2 rotate-45 border-b border-r border-main-green bg-main-gray"></span>
+            </p>
+            <div className="flex flex-col mt-5 mx-11">
+              <span className="text-main-green">John Smith</span>
+              <span className="text-white">Marketing Director at XYZ Corp</span>
+            </div>
+          </div>
+          <div className="hidden flex-col p-8 left-20 relative md:flex">
+            <p className="relative max-w-128 p-10 rounded-2xl border border-main-green text-white">
+              "We have been working with Positivus for the past year and have
+              seen a significant increase in website traffic and leads as a
+              result of their efforts. The team is professional, responsive, and
+              truly cares about the success of our business. We highly recommend
+              Positivus to any company looking to grow their online presence."
+              <span className="absolute w-[1rem] h-[1rem] left-10 -bottom-2 rotate-45 border-b border-r border-main-green bg-main-gray"></span>
+            </p>
+            <div className="flex flex-col mt-5 mx-11">
+              <span className="text-main-green">John Smith</span>
+              <span className="text-white">Marketing Director at XYZ Corp</span>
+            </div>
+          </div>
+        </div>
         <div className="flex flex-col md:flex-row mt-32 gap-5">
           <span className="bg-main-green w-fit max-w-52 text-center text-3xl font-semibold p-2 rounded relative mx-auto md:mx-0 md:max-w-fit md:text-start">
             Contact Us
